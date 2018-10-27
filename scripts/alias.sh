080e6f624afb517125063b609df023116dbd35d0
BlueLemon1992 <592591@gmail.com>
2018-10-28 01:50:33 +0200

    Update scripts/alias.sh
    add alias for connect the other servers directly
    add grep Agr()
    add cdArchive_G() to cd the archive folder of an agreement on GW
    add cdArchive_A() to cd the archive folder of process on APP
    add 0byte() to check empty files in archive folder, prot log and ftp log
    
    update massinfo() to minus output lines
    update hk() to beautify the output
    update ref() to beautify the outputs

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -9,6 +9,15 @@ alias old='ls -lt | tail';
 alias grep='ggrep --color=auto';
 alias vi='vim';
 alias xfb='cd /ext/schenker/logarchive/';
+alias a1='ssh -o StrictHostKeyChecking=no xibapprd1.dc.signintra.com';
+alias a2='ssh -o StrictHostKeyChecking=no xibapprd2.dc.signintra.com';
+alias a3='ssh -o StrictHostKeyChecking=no xibapprd3.dc.signintra.com';
+alias a4='ssh -o StrictHostKeyChecking=no xibapprd4.dc.signintra.com';
+alias g1='ssh -o StrictHostKeyChecking=no xibgwprd1.dc.signintra.com';
+alias g2='ssh -o StrictHostKeyChecking=no xibgwprd2.dc.signintra.com';
+alias g3='ssh -o StrictHostKeyChecking=no xibgwprd3.dc.signintra.com';
+alias g4='ssh -o StrictHostKeyChecking=no xibgwprd4.dc.signintra.com';
+alias FTP='ssh -o StrictHostKeyChecking=no amtrix@xibftprd1.dc.signintra.com';
 
 #values
 houseKeepPath=$sup/error/$(date "+%Y/%m/");
@@ -32,14 +41,30 @@ function getBDID()
 
 function massinfo()
 {
-    echo -n "number of $1:";
-    ls -l | grep $1 | grep -c ".att$";
-    ls -l | grep $1 | head;
+    echo -n "MassFilterFileNumber of $1:";
+    ls -l | grep "MassFilter" | grep $1 | grep -c ".att$";
+    ls -l | grep "MassFilter" | grep $1 | head;
 }
 
-function massclean()
+function runmass()
 {
-    echo "TODO";
+    . $FRAME_ROOT/tools/ScriptFunctions;
+    if [[ $1 != '' ]]; then
+        massinfo $1;
+        INTEGRATIONTYPE=$(SQLselect INTEGRATIONTYPE XIB_PROCESSIDPROPERTIES PROCESSIDCODE $1 | sed 's#^.* ##');
+        echo "Integration type : ${INTEGRATIONTYPE}";
+        if [[ "${INTEGRATIONTYPE}" == "Parallel" ]];then
+            echo -e "\n>>>deletemassfilterflag $1";
+            echo | deletemassfilterflag $1;
+            echo -e "\n>>>remafi $1 2";
+            remafi $1 2;
+        elif [[ "${INTEGRATIONTYPE}" == "Serial" ]]; then
+            echo -e "\n>>>remafi $1 6";
+            remafi $1 6;
+            echo -e "\n>>>deletemassfilterflag $1";
+            echo | deletemassfilterflag $1;
+        fi
+    fi
 }
 
 function download()
@@ -102,7 +127,9 @@ function hk()
 {
     if [[ $1 != '' ]]; then
         processID=$1;
-        ls -l | grep ${processID};
+        echo -n "ErrorFileNumber of $1:";
+        ls -l | grep -v "MassFilter" | grep $1 | grep -c ".att$";
+        ls -l | grep ${processID} | head;
         ref ${processID};
         /opt/sfw/bin/mv -v ${processID}* ${houseKeepPath};
     fi
@@ -143,17 +170,43 @@ function cdAgr()
     fi
 }
 
+function grepAgr()
+{
+    echo "TODO";
+    #will simplify findAgreement.sh to this function
+}
+
+function cdArchive_G()
+{
+    if [[ $1 != '' ]]; then
+        cd /ext/comsys*/archive/scheduler/$1/;
+        cd $(date +"%Y/%m/%d");
+    fi
+}
+
+function cdArchive_A()
+{
+    if [[ $1 != '' ]]; then
+        processID=$1;
+        if [[ $2 != '' ]];then
+            cd "/archive/${processID:0:4}/${processID:4:10}/IN/$(/opt/sfw/bin/date -d "1 day ago" +"%Y/%m/%d")";
+        else
+            cd "/ext/schenker/archive/${processID:0:4}/${processID:4:10}/IN/$(date +"%Y/%m/%d")";
+        fi
+    fi
+}
+
 function ref()
 {
-    for file in $(ls /ext/schenker/data/error/*$1*.att)
+    for file in $(ls /ext/schenker/data/error/*$1*.att | grep -v "MassFilter")
     do
         echo; basename ${file};
-        transaction=$(ggrep -A1 -E 'TransactionAttribute' ${file} | sed -n '2p');
+        transaction=$(ggrep -A1 -E 'TransactionAttribute' ${file} | grep -v "^TransactionAttribute$" | head -1);
         echo "Date/Time : $(echo ${transaction} | ggrep -Eo "[0-9]{4}-[a-zA-Z]{3}-[0-9]{2} [0-9:]{8}") CET";
         echo "TRID : $(ggrep -A1 -E 'TRID' ${file} | sed -n '2p')";
-        BDIDRefValues=$(ggrep -A1 -E 'BDIDRefValues' ${file} | sed -n '2p');
+        BDIDRefValues=$(ggrep -A1 -E 'BDIDRefValues' ${file} | grep -v "^BDIDRefValues$" | head -1);
         if [[ ${BDIDRefValues} != '' ]];then
-            echo ${BDIDRefValues} | awk -F\" '{print "BDID : "$2}';
+            echo ${BDIDRefValues:0:99} | awk -F\" '{print "BDID : "$2}';
             echo "BDIDRefValues : ";
             echo $BDIDRefValues | grep -Eo "\{[^\{\}]*\}" | awk -F\" '{print $2" : "$4}'
         fi
@@ -163,30 +216,43 @@ function ref()
     echo;
 }
 
-function checkResend()
+function 0byte()
 {
-    cd /ext/comsys0009/archive/scheduler/CISCUCEOUT_inbound/2018/10/08;
-    for file in $(gfind . -size 0);
+    for emptyFile in $(gfind $err -size 0 -name "*$1*" -type f -exec basename {} \;)
     do
-        ori=$(echo $file | cut -d\. -f4-);
-        count=$(gfind . -name "*$ori" -size +0 | wc -l | bc);
-        echo -n "$ori;";
-    if [[ $count == 0 ]];then
-        echo NO;
-    else
-        echo OK;
-    fi
+        fileName=$(basename ${emptyFile});
+        agrName=$(echo ${fileName} | cut -d\. -f1);
+        oriName=$(echo ${fileName} | cut -d\. -f3-);
+        archiveFolder="/ext/comsys*/archive/scheduler/${agrName}/$(date +"%Y/%m/%d")";
+        echo "0 byte file name : ${fileName}";
+        echo -e "\n===checking file with the same name in archive folder(${archiveFolder})===";
+        echo "0 byte file received : ";
+        gfind ${archiveFolder} -name "*${oriName}" -size 0 -type f -exec ls -l {} \;;
+        echo "file with content : ";
+        gfind ${archiveFolder} -name "*${oriName}" -size +0 -type f -exec ls -l {} \;;
+
+        comsysLogFile="/ext/comsys*/agr/${agrName}/../../log/log";
+        echo -e "\n===checking comsys log===";
+        grep ${oriName} ${comsysLogFile};
+
+        echo -e "\n===checking ftp access log===";
+        echo -n "Please input the keyword you want to use for grep ftp log, original filename (${oriName}) will be used if nothing was input:";
+        read -t 30 keywordForFTP;
+        if [[ ${keywordForFTP} == "" ]];then
+            keywordForFTP=${oriName};
+        fi
+        ssh -o StrictHostKeyChecking=no amtrix@xibftprd1.dc.signintra.com "/opt/amtcftp/tools/support/bin/ggrep ${keywordForFTP} /ext/schenker/prot/proftpd/proftpd.access_log";
     done
 }
 
 function info()
 {
     echo "alias:";
-    echo "..|...|ll|l|allen|new|old|grep|vi|xfb";
+    echo "..|...|ll|l|allen|new|old|grep|vi|xfb|a1|a2|a3|a4|g1|g2|g3|g4|FTP";
     echo "values:";
     echo "houseKeepPath|downloadPath|allen|ftpLog";
     echo "functions:";
-    echo "info|getBDID|massinfo|download|downloadError|monitor|catError|checkDEA|statByTime|statByAgrOrPID|sameFile|hk|clean|loop|oldFile|catAgr|cdAgr|ref";
+    echo "info|getBDID|massinfo|download|downloadError|monitor|catError|checkDEA|statByTime|statByAgrOrPID|sameFile|hk|clean|loop|oldFile|catAgr|cdAgr|ref|0byte|cdArchive_G|cdArchive_A";
 }
 
 
