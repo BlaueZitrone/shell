e1838471541b68107742df9dcc87c02c463d8297
BlueLemon1992 <592591@gmail.com>
2019-05-24 14:21:14 +0200

    update ftp_connection_detect

--- scripts/checkRemoteFTP/command.sh
+++ scripts/checkRemoteFTP/command.sh
@@ -4,3 +4,7 @@ curl --list-only ftp://<user>:<passwd>@<hostname>/<dir>/
 
 curl sftp://haftexternal.dell.com/skr_to_dell/pod/SKRSKRZA1595544.SWY -u haftmtgu0006
 
+/ext/comsys3001/bin/ftptls -vn -z certsok -z private isuzuphprod
+
+grep spaintir /etc/hosts
+
--- scripts/connection_test/ftp_connection_detect.sh
+++ scripts/connection_test/ftp_connection_detect.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-function FTPURLGen()
-{
-    hostname=$(grep '<RemoteHost>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-    user=$(grep '<User>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-    passwd=$(grep '<Password>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-    url="ftp://${user}:${passwd}@${hostname}";
-    echo "${url}" >> "${connectionURLTmpFile}";
-    echo "${agreementName}|${url}" >> "${agreementURLMap}";
-}
-
-function SFTPURLGen()
-{
-    hostname=$(grep '<RemoteHost>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-    user=$(grep '<User>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-    passwd=$(grep '<Password>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-    echo "sftp:${agreementName}:${hostname}:${user}:${passwd}";
-}
-
-function FTPSURLGen()
-{
-    echo "ftps:${agreementName}";
-}
-
-function connectionTest()
+function connectionDetect()
 {
     for URL in $(cat "${connectionURLTmpFile}")
     do
-        echo -n "${URL}";
+        echo -n "${URL} ";
         echo "${URL}" >> "${logFile}";
         curl "${URL}" --connect-timeout "${timeOutSec}" >> "${logFile}" 2>&1;
         if [[ $(echo $?) == '0' ]];then
@@ -35,7 +12,7 @@ function connectionTest()
         else
             echo -e "\033[1;31;5mFail\033[0m";
         fi
-        for agr in $(grep "|${URL}$" "${agreementURLMap}" | cut -d \| -f1)
+        for agr in $(fgrep "|${URL}" "${agreementURLMap}" | cut -d \| -f1)
         do
             echo -n "${agr}|";
         done
@@ -49,36 +26,30 @@ function init()
     connectionURLTmpFile="/tmp/connectionURL_$(date "+%Y%m%d%H%M%S").tmp";
     agreementURLMap="/tmp/agreementURL_$(date "+%Y%m%d%H%M%S").map";
     logFile="$(dirname $0)/log.$(date "+%Y%m%d")";
-    gfind "/tmp/" -mtime +1 -name "connectionURL_*.tmp" -exec /usr/bin/rm {} \; 2>/dev/null;
-    gfind "/tmp/" -mtime +1 -name "agreementURL_*.map" -exec /usr/bin/rm {} \; 2>/dev/null;
-    gfind "$(dirname $0)/" -mtime +10 -name "log.*" -exec /usr/bin/rm {} \; 2>/dev/null;
+    find "/tmp/" -mtime +1 -name "connectionURL_*.tmp" -exec /usr/bin/rm {} \; 2>/dev/null;
+    find "/tmp/" -mtime +1 -name "agreementURL_*.map" -exec /usr/bin/rm {} \; 2>/dev/null;
+    find "$(dirname $0)/" -mtime +10 -name "log.*" -exec /usr/bin/rm {} \; 2>/dev/null;
 }
 
 function main()
 {
     init;
-    for agreementConfigFile in $(ls /ext/comsys*/agr/*/*_dump.xml)
+    for agreementConfigFile in $(ls /app/sword/schenker/comsys/COMSYS*/agr/*/*_dump.xml)
     do
         protocol=$(grep '<Protocol>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
         agreementName=$(grep '<AgrID>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
-        case "${protocol}" in
-            ftp)
-            FTPURLGen;
-            ;;
-            script)
-            SFTPURLGen;
-            ;;
-            ftps)
-            FTPSURLGen;
-            ;;
-            *)
-            echo "Failed to check agreement ${agreementName} cause undefined protocol ${protocol}.";
-            ;;
-        esac
+        if [[ "${protocol}" == "ftp" ]]; then
+            hostname=$(grep '<RemoteHost>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
+            user=$(grep '<User>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
+            passwd=$(grep '<Password>' ${agreementConfigFile} | sed "s/<[^<>]*>//g");
+            url="ftp://${user}:${passwd}@${hostname}";
+            echo "${url}" >> "${connectionURLTmpFile}";
+            echo "${agreementName}|${url}" >> "${agreementURLMap}";
+        fi
     done
     #remove duplicate connection URLs
     cat "${connectionURLTmpFile}" | sort | uniq > "${connectionURLTmpFile}";
-    connectionTest;
+    connectionDetect;
     /usr/bin/rm "${connectionURLTmpFile}";
     /usr/bin/rm "${agreementURLMap}";
 }
