ef57a025e2ef2a8d876cd014445c1764df8f17bb
Allen Chen <allen.a.chen@dbschenker.com>
2020-05-14 18:19:46 +0800

    finish the version 1.0 of the cuplicate check script

--- python/check_duplicate_rates/check.py
+++ python/check_duplicate_rates/check.py
@@ -1,11 +1,24 @@
-
 import xlrd
 
-workbook=xlrd.open_workbook("testdata.xls")
-#print(workbook.sheet_names())
-worksheet=workbook.sheet_by_name("testdata")
-print(worksheet.name)
-nrows=worksheet.nrows
-#print(nrows)
-for key_ncol in [3,7,10,13,15]:
-    print(worksheet.col_values(key_ncol), end="\n")
+filename=input("Please input the excel file you want to check : ")
+workBook=xlrd.open_workbook(filename)
+workBook=xlrd.open_workbook("sample.xls")
+
+keySheet=workBook.sheet_by_name("Key")
+valueSheet=workBook.sheet_by_name("Value")
+
+listOfKeys=[";".join(keySheet.row_values(i)).replace(" ","").lower() for i in range(1, keySheet.nrows)]
+listofValues=[float("0"+str(valueSheet.cell_value(i,0))) for i in range(1,valueSheet.nrows)]
+uniqKeys=set()
+duplicateKeys=set()
+for element in listOfKeys:
+    if element in uniqKeys:
+        duplicateKeys.add(element)
+    else:
+        uniqKeys.add(element)
+
+for duplicateKey in duplicateKeys:
+    print(duplicateKey)
+    for i,x in enumerate(listOfKeys):
+        if x == duplicateKey:
+            print("    Row: "+str(i+1)+" Value: "+str(listofValues[i]))
